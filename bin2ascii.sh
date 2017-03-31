4e699a3bcbbb692fccb30bcfc5baff3bfb56fce7
krzysztof.krzeszowski <chriss@astro.ia.uz.zgora.pl>
2017-03-31 13:40:29 +0200

    dir/file tests

--- bin2ascii.sh
+++ bin2ascii.sh
@@ -1,1 +1,39 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+# check number of parameters
+if [[ $# -ne 2 ]]; then
+    echo "Usage:"
+    echo "./bin2ascii source_directory path_to_out_file"
+    exit 1
+fi
+
+# does source directory exist?
+if [[ ! -d $1 ]]; then
+    echo "input directory does not exist"
+    exit 1
+fi
+
+# is file name provided
+if [[ -d $2 ]]; then
+    echo "provide output file name"
+    exit 1
+fi
+
+# does output directory exist?
+if [[ ! -d `dirname $2` ]]; then
+    echo "output directory does not exist"
+    exit 1
+fi
+
+# does output file exist?
+if [[ -f $2 ]]; then
+    read -p "output file exists, overwrite? [y/N] " -n 1 -r
+    echo
+    if [[ ! $REPLY == "y" ]]; then
+        exit 1
+    fi
+fi
+
+command -v psrtxt >/dev/null 2>&1 || { echo >&2 "psrtxt not found. Please add to PATH"; exit 1; }
+
+echo `ls -a $1/*.ar | wc -l`
